7750a56743805cfec37b30ae56b27ccd6fedf055
imane ELKADI <imaneelkadi22@gmail.com>
2023-02-18 00:03:22 +0100

    validation psw

--- valider.php
+++ valider.php
@@ -3,13 +3,9 @@ require "connection.php";
 
 $user=$_POST['user'];
 $psw=$_POST['psw'];
-$req="insert into user values('$user','$psw')";
-$req2="select user,password from newclient where user='$user' and password='$psw'";
-$a=$cnx->query($req);
-$b=$cnx->query($req2);
-if(($a==true)&&($b==true))
-echo" ok";
-else
-echo" not ok";
-
+$user_check = $cnx->query("SELECT user FROM  user WHERE user='$user'");
+$psw_check = $cnx->query("SELECT password FROM  user WHERE password='$psw'");
+if ($psw_check->num_rows==0 || $user_check->num_rows==0) 
+    
+    echo'<h3>Sorry</h3>That username  or the password does not exist in our database.<h3>create new account</h3>';
 ?>
