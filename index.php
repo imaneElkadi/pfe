eb5f33a1e88ad60dd1b71ef7b0881ee533865266
imane ELKADI <imaneelkadi22@gmail.com>
2023-02-17 22:28:24 +0100

    add 1st form

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>our pfe -- ELKADI Imane . SOUIDI Khaoula</title>
+</head>
+<body>
+    <fieldset style="width:100px">
+    <form action="valider.php" method="post">
+<div>have an account</div>
+<table>
+    <tr>
+ <td><label for="user"> User</label></td>
+ <td><input type="text" placeholder="User" name="user"></td>
+</tr>
+<tr>
+ <td><label for="psw"> User</label></td>
+ <td><input type="password" placeholder="Password" name="psw"></td>
+</tr>
+<tr>
+ 
+ <td><input type="submit"  name="entrer" value="Enter"></td>
+</tr>
+</table>
+    </form>
+    </fieldset>
+</body>
+</html>
