c28f0f8bc74a56490b15cb7ad1b2b0bb84dee8e1
imane ELKADI <imaneelkadi22@gmail.com>
2023-02-17 23:40:16 +0100

    ,,

--- valider.php
+++ valider.php
@@ -3,9 +3,13 @@ require "connection.php";
 
 $user=$_POST['user'];
 $psw=$_POST['psw'];
-$req="insert into user values('$fn','$email')";
-if($cnx->query($req)==true)
-echo"insertion ok";
+$req="insert into user values('$user','$psw')";
+$req2="select user,password from newclient where user='$user' and password='$psw'";
+$a=$cnx->query($req);
+$b=$cnx->query($req2);
+if(($a==true)&&($b==true))
+echo" ok";
 else
-echo"insertion not ok";
+echo" not ok";
+
 ?>
